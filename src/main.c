a6bcd6e6c8dd651f7d93484fc20b97c63d519839
nexustix <nexus1768@tuta.io>
2020-04-24 03:28:47 +0200

    Add implement #include

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "bignum.h"
 #include "dictionary.h"
@@ -8,6 +9,10 @@
 #include "util.h"
 #include "vm.h"
 #include "vocabularies/math/arithmetic.h"
+
+// HACK
+static char *content_root;
+// char *get_content_root() { return content_root; }
 
 void _spam(VM *self) { printf("Spam\n"); }
 void _wonderful(VM *self) { printf("Wonderful Spam\n"); }
@@ -70,6 +75,55 @@ void _enter(VM *self) {
 
 void _leave(VM *self) { vm_pop_namespace(self); }
 
+// HACK
+void _include(VM *self) {
+  static Element *e;
+  if (!e) e = newElement();
+
+  vm_pop_value(self, e);
+  assert(e->kind == ELEMENT_BIGNUM);
+  const char *sub_path = bignum_to_cstring(element_get_bignum(e));
+
+  size_t content_root_length = strlen(content_root);
+  size_t sub_path_length = strlen(content_root);
+
+  char *complete_path =
+      calloc(content_root_length + sub_path_length + strlen(".rcn") + 1, 1);
+
+  strcat(complete_path, content_root);
+  strcat(complete_path, "/");
+  strcat(complete_path, sub_path);
+  strcat(complete_path, ".rcn");
+
+  List *tokens = newList(16);
+  FILE *handle = NULL;
+  size_t size;
+  char *data = malloc(1024);
+  size_t tbd;
+
+  // printf(">%s<\n", complete_path);
+  handle = fopen(complete_path, "r");
+
+  if (handle) {
+    int firstline = 1;
+    while ((tbd = getline(&data, &size, handle)) != -1) {
+      // printf(">%s<\n", data);
+      if (firstline) {
+        firstline = 0;
+        // shebang support
+        if (strncmp(data, "#!", strlen("#!")) == 0) {
+          // if (strncmp(data, "#", strlen("#")) == 0) {
+          continue;
+        }
+      }
+      parse_tokens(data, tokens);
+      vm_execute_all(self, tokens, 0);
+      tokens->top = 0;
+    }
+    fclose(handle);
+  }
+}
+
 void register_words(VM *vm) {
   vm_add_primary(vm, "def", _def);
   vm_add_compile(vm, "def", _def_compile);
@@ -87,6 +141,8 @@ void register_words(VM *vm) {
   vm_add_primary(vm, "#enter", _enter);
   vm_add_primary(vm, "#leave", _leave);
 
+  vm_add_primary(vm, "#include", _include);
+
   vm_add_primary(vm, "spam", _spam);
   vm_add_primary(vm, "cake.spam", _spam);
   vm_add_primary(vm, "wonderful", _wonderful);
@@ -98,18 +154,36 @@ int main(int argc, const char *argv[]) {
   char *data = malloc(1024);
   size_t tbd;
 
+  // char *content_root = getenv("RCNPATH");
+  content_root = getenv("RCNPATH");
+
+  if (!content_root) {
+    // FIXME questionable constant
+    content_root = malloc(4096);
+    getcwd(content_root, 4096);
+  }
+
   VM *vm = newVM();
   register_words(vm);
 
   FILE *handle = NULL;
 
-  // printf("argc >%d<\n", argc);
+  printf("argc >%s<\n", content_root);
 
   if (argc > 1) {
     handle = fopen(argv[1], "r");
   }
   if (handle) {
+    int firstline = 1;
     while ((tbd = getline(&data, &size, handle)) != -1) {
+      if (firstline) {
+        firstline = 0;
+        // shebang support
+        if (strncmp(data, "#!", strlen("#!")) == 0) {
+          // if (strncmp(data, "#", strlen("#")) == 0) {
+          continue;
+        }
+      }
       parse_tokens(data, tokens);
       vm_execute_all(vm, tokens, 0);
       tokens->top = 0;
