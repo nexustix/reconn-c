542459c7e53473db495f4f903baea2b64af57dc7
nexustix <nexus1768@tuta.io>
2020-08-22 03:17:29 +0200

    Change improve stack implementation

--- src/head/reconn/stack/stack.h
+++ src/head/reconn/stack/stack.h
@@ -1,6 +1,7 @@
 #ifndef RECONN_STACK_STACK_H
 #define RECONN_STACK_STACK_H
 
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -44,7 +45,7 @@ ReconnStack reconn_makeStack() {
 }
 
 void reconn_stack_print(ReconnStack *self) {
-  printf("TOP:%i\n", self->top);
+  printf("======\nTOP:%i\n", self->top);
   for (unsigned int i = 0; i < self->count; i++) {
     printf("(%5i:%5i)\n", self->items[i].start, self->items[i].stop);
     for (unsigned int si = self->items[i].start; si < self->items[i].stop;
@@ -62,124 +63,85 @@ ADD ITEMS
 =========
 */
 
+static void reconn_stack_finalize_push(ReconnStack *self,
+                                       ReconnStackItemKind kind,
+                                       unsigned short size) {
+  self->items[self->count].kind = kind;
+  self->items[self->count].start = self->top;
+  self->items[self->count].stop = self->top + size;
+
+  self->top += size;
+  self->count += 1;
+}
+
 void reconn_stack_push_u8(ReconnStack *self, unsigned char value) {
   unsigned char *location = (unsigned char *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_U8;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_U8, sizeof(value));
 };
 
 void reconn_stack_push_u16(ReconnStack *self, unsigned short value) {
   unsigned short *location = (unsigned short *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_U16;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_U16, sizeof(value));
 };
 
 void reconn_stack_push_u32(ReconnStack *self, unsigned long value) {
   unsigned long *location = (unsigned long *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_U32;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_U32, sizeof(value));
 };
 
 void reconn_stack_push_u64(ReconnStack *self, unsigned long long value) {
   unsigned long long *location = (unsigned long long *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_U64;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_U64, sizeof(value));
 };
 
 void reconn_stack_push_s8(ReconnStack *self, char value) {
   char *location = (char *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_S8;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_S8, sizeof(value));
 };
 
 void reconn_stack_push_s16(ReconnStack *self, short value) {
   short *location = (short *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_S16;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_S16, sizeof(value));
 };
 
 void reconn_stack_push_s32(ReconnStack *self, long value) {
   long *location = (long *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_S32;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_S32, sizeof(value));
 };
 
 void reconn_stack_push_s64(ReconnStack *self, long long value) {
   long long *location = (long long *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_S64;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_S64, sizeof(value));
 };
 
 void reconn_stack_push_f32(ReconnStack *self, float value) {
   float *location = (float *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_F32;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_F32, sizeof(value));
 };
 
 void reconn_stack_push_f64(ReconnStack *self, double value) {
   double *location = (double *)&self->data[self->top];
   *location = value;
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_F64;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + sizeof(value);
-
-  self->top += sizeof(value);
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_F64, sizeof(value));
 };
 
 void reconn_stack_push_cstring(ReconnStack *self, char *value) {
@@ -187,26 +149,15 @@ void reconn_stack_push_cstring(ReconnStack *self, char *value) {
   char *location = (char *)&self->data[self->top];
   memcpy(location, value, length);
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_C_STRING;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + length;
-
-  self->top += length;
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_C_STRING, length);
 }
 
 void reconn_stack_push_string(ReconnStack *self, char *value,
                               unsigned short length) {
-
   char *location = (char *)&self->data[self->top];
   memcpy(location, value, length);
 
-  self->items[self->count].kind = RECONN_STACK_ITEM_BYTE_STRING;
-  self->items[self->count].start = self->top;
-  self->items[self->count].stop = self->top + length;
-
-  self->top += length;
-  self->count += 1;
+  reconn_stack_finalize_push(self, RECONN_STACK_ITEM_BYTE_STRING, length);
 }
 
 /*
@@ -215,129 +166,74 @@ REMOVE ITEMS
 ============
 */
 
-unsigned char reconn_stack_pop_u8(ReconnStack *self) {
+void *reconn_stack_pop_void(ReconnStack *self) {
   unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
+  unsigned short size =
       self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  unsigned char *location = (unsigned char *)&self->data[start];
+
+  void *location = (void *)&self->data[start];
 
   self->top -= size;
   self->count -= 1;
 
-  return *location & 0xff;
+  return location;
+}
+
+unsigned char reconn_stack_pop_u8(ReconnStack *self) {
+  unsigned char *location = (unsigned char *)reconn_stack_pop_void(self);
+  return *location & UCHAR_MAX;
 }
 
 unsigned short reconn_stack_pop_u16(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  unsigned short *location = (unsigned short *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffff;
+  unsigned short *location = (unsigned short *)reconn_stack_pop_void(self);
+  return *location & USHRT_MAX;
 }
 
 unsigned long reconn_stack_pop_u32(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  unsigned long *location = (unsigned long *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffffffff;
+  unsigned long *location = (unsigned long *)reconn_stack_pop_void(self);
+  return *location & ULONG_MAX;
 }
 
 unsigned long long reconn_stack_pop_u64(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  unsigned long long *location = (unsigned long long *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffffffffffffffff;
+  unsigned long long *location =
+      (unsigned long long *)reconn_stack_pop_void(self);
+  return *location & ULLONG_MAX;
 }
 
 char reconn_stack_pop_s8(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  char *location = (char *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xff;
+  char *location = (char *)reconn_stack_pop_void(self);
+  return *location & UCHAR_MAX;
 }
 
 short reconn_stack_pop_s16(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  short *location = (short *)&self->data[start];
+  short *location = (short *)reconn_stack_pop_void(self);
 
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffff;
+  return *location & USHRT_MAX;
 }
 
 long reconn_stack_pop_s32(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  long *location = (long *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffffffff;
+  long *location = (long *)reconn_stack_pop_void(self);
+  return *location & ULONG_MAX;
 }
 
 long long reconn_stack_pop_s64(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  long long *location = (long long *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
-  return *location & 0xffffffffffffffff;
+  long long *location = (long long *)reconn_stack_pop_void(self);
+  return *location & ULLONG_MAX;
 }
 
 float reconn_stack_pop_f32(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  float *location = (float *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
+  float *location = (float *)reconn_stack_pop_void(self);
   return *location;
 }
 
 double reconn_stack_pop_f64(ReconnStack *self) {
-  unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
-      self->items[self->count - 1].stop - self->items[self->count - 1].start;
-  double *location = (double *)&self->data[start];
-
-  self->top -= size;
-  self->count -= 1;
-
+  double *location = (double *)reconn_stack_pop_void(self);
   return *location;
 }
 
 void reconn_stack_pop_cstring(ReconnStack *self, char *dest) {
   unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
+  unsigned short size =
       self->items[self->count - 1].stop - self->items[self->count - 1].start;
   char *location = (char *)&self->data[start];
 
@@ -349,7 +245,7 @@ void reconn_stack_pop_cstring(ReconnStack *self, char *dest) {
 
 void reconn_stack_pop_string(ReconnStack *self, char *dest) {
   unsigned char start = self->items[self->count - 1].start;
-  unsigned char size =
+  unsigned short size =
       self->items[self->count - 1].stop - self->items[self->count - 1].start;
   double *location = (double *)&self->data[start];
 
