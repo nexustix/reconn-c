c13bc39206e209edcba63d4475741a149f88e3f3
nexustix <nexus1768@tuta.io>
2020-08-28 01:24:19 +0200

    Change clean up division words

--- src/head/reconn/module/math.h
+++ src/head/reconn/module/math.h
@@ -20,6 +20,8 @@ int reconn_mod_math_add(ReconnVM *vm) {
   const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
 
   if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
   }
 
   if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
@@ -50,6 +52,8 @@ int reconn_mod_math_sub(ReconnVM *vm) {
   const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
 
   if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
   }
 
   if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
@@ -80,6 +84,8 @@ int reconn_mod_math_mul(ReconnVM *vm) {
   const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
 
   if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
   }
 
   if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
@@ -98,7 +104,7 @@ int reconn_mod_math_mul(ReconnVM *vm) {
   return 0;
 }
 
-int reconn_mod_math_idiv(ReconnVM *vm) {
+int reconn_mod_math_rdiv(ReconnVM *vm) {
   if (!(reconn_buffer_count(&vm->value_stack) > 0)) {
     vm->got_error = 1;
     return -1;
@@ -110,44 +116,18 @@ int reconn_mod_math_idiv(ReconnVM *vm) {
   const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
 
   if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
   }
 
   if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
-    double b = reconn_value_to_f64(kind_b, value_b);
-    double a = reconn_value_to_f64(kind_a, value_a);
-    // reconn_buffer_push_f64(&vm->value_stack, a / b);
-    reconn_buffer_push_f64(&vm->value_stack, floor((a / b) + 0.5));
-  } else if (reconn_value_is_signed(kind_a) || reconn_value_is_signed(kind_b)) {
+    // double b = reconn_value_to_f64(kind_b, value_b);
+    // double a = reconn_value_to_f64(kind_a, value_a);
+    // reconn_buffer_push_f64(&vm->value_stack, floor((a / b) + 0.5));
     long long b = reconn_value_to_s64(kind_b, value_b);
     long long a = reconn_value_to_s64(kind_a, value_a);
     reconn_buffer_push_s64(&vm->value_stack, a / b);
-  } else {
-    long long b = reconn_value_to_u64(kind_b, value_b);
-    long long a = reconn_value_to_u64(kind_a, value_a);
-    reconn_buffer_push_u64(&vm->value_stack, a / b);
-  }
-  return 0;
-}
-
-int reconn_mod_math_div(ReconnVM *vm) {
-  if (!(reconn_buffer_count(&vm->value_stack) > 0)) {
-    vm->got_error = 1;
-    return -1;
-  }
-  const ReconnValueKind kind_b = reconn_buffer_kind_at(&vm->value_stack, -1);
-  const ReconnValueKind kind_a = reconn_buffer_kind_at(&vm->value_stack, -2);
-
-  const void *value_b = reconn_buffer_pop_void(&vm->value_stack);
-  const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
-
-  if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
-  }
-
-  if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
-    double b = reconn_value_to_f64(kind_b, value_b);
-    double a = reconn_value_to_f64(kind_a, value_a);
-    reconn_buffer_push_f64(&vm->value_stack, a / b);
-    reconn_buffer_push_f64(&vm->value_stack, 0);
+    reconn_buffer_push_s64(&vm->value_stack, a % b);
   } else if (reconn_value_is_signed(kind_a) || reconn_value_is_signed(kind_b)) {
     long long b = reconn_value_to_s64(kind_b, value_b);
     long long a = reconn_value_to_s64(kind_a, value_a);
@@ -159,7 +139,6 @@ int reconn_mod_math_div(ReconnVM *vm) {
     reconn_buffer_push_u64(&vm->value_stack, a / b);
     reconn_buffer_push_u64(&vm->value_stack, a % b);
   }
-  return 0;
   return 0;
 }
 
@@ -175,6 +154,8 @@ int reconn_mod_math_fdiv(ReconnVM *vm) {
   const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
 
   if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
   }
 
   double b = reconn_value_to_f64(kind_b, value_b);
@@ -184,6 +165,45 @@ int reconn_mod_math_fdiv(ReconnVM *vm) {
   return 0;
   return 0;
 }
+
+int reconn_mod_math_idiv(ReconnVM *vm) {
+  if (!(reconn_buffer_count(&vm->value_stack) > 0)) {
+    vm->got_error = 1;
+    return -1;
+  }
+  const ReconnValueKind kind_b = reconn_buffer_kind_at(&vm->value_stack, -1);
+  const ReconnValueKind kind_a = reconn_buffer_kind_at(&vm->value_stack, -2);
+
+  const void *value_b = reconn_buffer_pop_void(&vm->value_stack);
+  const void *value_a = reconn_buffer_pop_void(&vm->value_stack);
+
+  if (!(reconn_value_is_number(kind_a) && reconn_value_is_number(kind_b))) {
+    vm->got_error = 1;
+    return -1;
+  }
+
+  if (reconn_value_is_float(kind_a) || reconn_value_is_float(kind_b)) {
+    // double b = reconn_value_to_f64(kind_b, value_b);
+    // double a = reconn_value_to_f64(kind_a, value_a);
+    // reconn_buffer_push_f64(&vm->value_stack, floor((a / b) + 0.5));
+    long long b = reconn_value_to_s64(kind_b, value_b);
+    long long a = reconn_value_to_s64(kind_a, value_a);
+    reconn_buffer_push_s64(&vm->value_stack, a / b);
+    // reconn_buffer_push_s64(&vm->value_stack, a % b);
+  } else if (reconn_value_is_signed(kind_a) || reconn_value_is_signed(kind_b)) {
+    long long b = reconn_value_to_s64(kind_b, value_b);
+    long long a = reconn_value_to_s64(kind_a, value_a);
+    reconn_buffer_push_s64(&vm->value_stack, a / b);
+    // reconn_buffer_push_s64(&vm->value_stack, a % b);
+  } else {
+    long long b = reconn_value_to_u64(kind_b, value_b);
+    long long a = reconn_value_to_u64(kind_a, value_a);
+    reconn_buffer_push_u64(&vm->value_stack, a / b);
+    // reconn_buffer_push_u64(&vm->value_stack, a % b);
+  }
+  return 0;
+}
+
 // int reconn_mod_math_mod(ReconnVM *vm) { return 0; }
 
 // int reconn_mod_math_(ReconnVM *vm) {
@@ -194,9 +214,9 @@ int reconn_mod_math_register_all(ReconnVM *vm) {
   reconn_vm_add_primary(vm, "+", reconn_mod_math_add);
   reconn_vm_add_primary(vm, "-", reconn_mod_math_sub);
   reconn_vm_add_primary(vm, "*", reconn_mod_math_mul);
-  reconn_vm_add_primary(vm, "/", reconn_mod_math_div);
-  reconn_vm_add_primary(vm, "/.", reconn_mod_math_fdiv);
+  reconn_vm_add_primary(vm, "/%", reconn_mod_math_rdiv);
   reconn_vm_add_primary(vm, "//", reconn_mod_math_idiv);
+  reconn_vm_add_primary(vm, "/", reconn_mod_math_fdiv);
   return 0;
 }
 
